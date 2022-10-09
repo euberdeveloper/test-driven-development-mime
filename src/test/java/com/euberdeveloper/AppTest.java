bf8676da9a64f7207c945a3ad5057d6354f1e9c8
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 22:12:41 +0200

    test: change to Money
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/test/java/com/euberdeveloper/AppTest.java
+++ src/test/java/com/euberdeveloper/AppTest.java
@@ -38,8 +38,8 @@ public class AppTest {
 
     @Test
     public void testMixinAddition() {
-        Expression fiveBucks = Money.dollar(5);
-        Expression tenFrancs = Money.franc(10);
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
