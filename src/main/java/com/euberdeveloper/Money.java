ffff8ed297af2c6c2bec6a0e3037301f8c80362f
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 15:00:19 +0200

    refactor(Money.java): use Money.franc instead of constructor
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main/java/com/euberdeveloper/Money.java
+++ src/main/java/com/euberdeveloper/Money.java
@@ -9,7 +9,7 @@ abstract public class Money {
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, null);
+        return Money.franc(amount);
     }
 
     abstract Money times(int multiplier);
