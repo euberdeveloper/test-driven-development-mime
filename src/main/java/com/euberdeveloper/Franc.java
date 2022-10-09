54aff64b4670874bc4f98b93664feadd4a01a49a
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 14:24:42 +0200

    fix(Franc.java): add Franc class
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/main/java/com/euberdeveloper/Franc.java
@@ -0,0 +1,19 @@
+package com.euberdeveloper;
+
+public class Franc {
+    private final int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return this.amount == franc.amount;
+    }
+
+}
