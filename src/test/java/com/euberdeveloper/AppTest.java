0c34072852771cbce14f5ffa6bd35fe97141a3e1
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 22:27:34 +0200

    refactor(test): remove unused import
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/test/java/com/euberdeveloper/AppTest.java
+++ src/test/java/com/euberdeveloper/AppTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class AppTest {
     @Test
