2cb5d1a2dee34828b88c28fb2f6604db5a0c916c
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 14:58:55 +0200

    refactor(Franc.java): add currency parameter to constructor
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main/java/com/euberdeveloper/Franc.java
+++ src/main/java/com/euberdeveloper/Franc.java
@@ -1,9 +1,9 @@
 package com.euberdeveloper;
 
 public class Franc extends Money {
-    public Franc(int amount) {
+    public Franc(int amount, String currency) {
         this.amount = amount;
-        this.currency = "CHF";
+        this.currency = currency;
     }
 
     public Money times(int multiplier) {
