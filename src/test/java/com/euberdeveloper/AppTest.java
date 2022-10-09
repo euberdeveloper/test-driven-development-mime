6e0266758910b0751c616b91f6bb28964db00b43
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 14:11:54 +0200

    test: add testEquality
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/test/java/com/euberdeveloper/AppTest.java
+++ src/test/java/com/euberdeveloper/AppTest.java
@@ -1,6 +1,7 @@
 package com.euberdeveloper;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -12,5 +13,10 @@ public class AppTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+       assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }
