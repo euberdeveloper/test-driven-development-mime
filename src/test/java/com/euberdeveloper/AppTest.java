90bff17ed2a9ca632bf27ee01cf852433e228254
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 17:17:32 +0200

    test: add  testReduceMoneyDifferentCurrencies
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/test/java/com/euberdeveloper/AppTest.java
+++ src/test/java/com/euberdeveloper/AppTest.java
@@ -59,4 +59,12 @@ public class AppTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrencies() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
