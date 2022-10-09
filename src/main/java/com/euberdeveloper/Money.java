d489598b8633eab0f6070acdbf0ecb1702aebe47
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 14:28:17 +0200

    refactor: move equals from Dollar to Money
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main/java/com/euberdeveloper/Money.java
+++ src/main/java/com/euberdeveloper/Money.java
@@ -2,4 +2,9 @@ package com.euberdeveloper;
 
 public class Money {
     protected int amount;
+
+    public boolean equals(Object object) {
+        Money dollar = (Money) object;
+        return this.amount == dollar.amount;
+    }
 }
