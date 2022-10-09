a25fb649f7d0cc2d91543212647ca6be5a04b1f7
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 16:48:01 +0200

    test: refactor testSimpleAddition to have Bank and Expression
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/test/java/com/euberdeveloper/AppTest.java
+++ src/test/java/com/euberdeveloper/AppTest.java
@@ -16,20 +16,23 @@ public class AppTest {
 
     @Test
     public void testEquality() {
-       assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-       assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-       assertFalse(Money.franc(5).equals(Money.dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     @Test
     public void testCurrency() {
-      assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
 
     @Test
     public void testSimpleAddition() {
-      Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertEquals(Money.dollar(10), sum);
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 }
