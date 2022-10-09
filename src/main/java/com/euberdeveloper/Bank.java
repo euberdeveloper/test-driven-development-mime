17f40081926da57cb508274641e8c89ecb4aeab3
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 17:27:10 +0200

    feat(Bank.java): add rates
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main/java/com/euberdeveloper/Bank.java
+++ src/main/java/com/euberdeveloper/Bank.java
@@ -1,11 +1,15 @@
 package com.euberdeveloper;
 
+import java.util.Hashtable;
+
 public class Bank {
-    Money reduce(Expression source, String to) {
+    public Hashtable<String, Pair> rates = new Hashtable<String, Pair>();
+
+    public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
-    int rate(String from, String to) {
+    public int rate(String from, String to) {
         return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
