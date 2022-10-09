0a27fd4b0b2b8100d04e19f5c25757e2931479f3
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 17:21:39 +0200

    refactor(src): change reduce signature
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main/java/com/euberdeveloper/Bank.java
+++ src/main/java/com/euberdeveloper/Bank.java
@@ -2,6 +2,6 @@ package com.euberdeveloper;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 }
