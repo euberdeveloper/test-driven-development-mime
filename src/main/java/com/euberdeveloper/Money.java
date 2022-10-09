4157e7934e4aa414dee5b54a0ea3f22d1d6cf01e
euberdeveloper <euberdeveloper@gmail.com>
2022-10-09 14:56:32 +0200

    refactor(Money.java): add currency field
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main/java/com/euberdeveloper/Money.java
+++ src/main/java/com/euberdeveloper/Money.java
@@ -2,20 +2,25 @@ package com.euberdeveloper;
 
 abstract public class Money {
     protected int amount;
+    protected String currency;
 
     public static Money dollar(int amount) {
         return new Dollar(amount);
     }
+
     public static Money franc(int amount) {
         return new Franc(amount);
     }
 
     abstract Money times(int multiplier);
-    abstract String currency();
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    public String currency() {
+        return this.currency;
     }
 
 }
